1d671e0effd030ae6d2ad548ab372a434360da9f
Atik Shahriar Ratul <ratul@Atiks-MacBook-Pro.local>
2024-10-29 01:43:35 +0600

    fix nextjs

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  server: {
-    port: parseInt(process.env.CLIENT_PORT || '3000', 10),
-  },
+
 };
 
 export default nextConfig;
